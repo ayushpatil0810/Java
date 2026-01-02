a74b914356c2886167f923f875b3075714cb864f
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-02 15:21:42 +0530

    Add Literals.java with examples of binary, hexadecimal, and character literals

---
+++ Literals.java
@@ -0,0 +1,13 @@
+public class Literals {
+    public static void main(String[] args) {
+        
+        int binaryNum = 0b0101; // output -> 5
+        int hexNum = 0x7E; // output -> 126
+
+        int x = 10_00_00_000; // 100000000
+
+        char c = 'a';
+        c++;
+        System.out.println(c); // b
+    }
+}
