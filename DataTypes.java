331912569af4a5e7ef46432744925c0a4fb63897
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-02 15:11:33 +0530

    Add DataTypes.java with basic data type examples

---
+++ DataTypes.java
@@ -0,0 +1,12 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 7892l;
+        float f = 5.8f;
+        double d = 5.8;
+        char c = 'k';
+        boolean b = true;
+    }
+}
