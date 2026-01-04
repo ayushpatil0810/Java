92602ac7b7c5e474cb5990a11ecfb86672e75893
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-04 22:27:59 +0530

    Add ArithmeticOperations.java with examples of arithmetic operations and increment/decrement operators

---
+++ ArithmeticOperations.java
@@ -0,0 +1,28 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int num1 = 7;
+        // int num2 = 5;
+
+        // int addition = num1 + num2;
+        // int subtraction = num1 - num2;
+        // int multiplication = num1 * num2;
+        // double division = (double) num1 / num2;
+        // int reminder = num1 % num2;
+
+        // System.out.println(addition);
+        // System.out.println(subtraction);
+        // System.out.println(multiplication);
+        // System.out.println(division);
+        // System.out.println(reminder);
+
+        num1 += 2; // num1 = num1 + 2
+        num1 *= 2; // num1 = num1 * 2
+        num1 -= 2; // num1 = num1 - 2
+        num1 /= 2; // num1 = num1 / 2
+
+        num1++; // post-increment
+        num1--; // post-decrement
+        ++num1; // pre-increment
+        --num1; // pre-decrement
+    }
+}
