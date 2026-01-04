58828051cde423966ee8686ca47dac333c6b4939
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-04 22:48:18 +0530

    Add additional examples of conditional statements in conditionals.java

--- conditionals.java
+++ conditionals.java
@@ -13,5 +13,18 @@ public class conditionals {
 
         // && -> and operator
         // || -> or operator
+
+
+        int a = 2;
+        int b = 6;
+        int c = 9;
+
+
+        if (a > b && a > c) 
+            System.out.println(a);
+        else if (b > a && b > c) 
+            System.out.println(b);
+        else 
+            System.out.println(c);
     }
 }
