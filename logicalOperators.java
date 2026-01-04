c3720def9ee3e7ee07b23791101c60a7ef691fbd
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-04 22:36:11 +0530

    Add logicalOperators.java with examples of logical operations

---
+++ logicalOperators.java
@@ -0,0 +1,12 @@
+public class logicalOperators {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 5;
+
+        boolean result = x < 5;
+
+        System.out.println(result);
+
+        // I know rest of it 😏
+    }
+}
