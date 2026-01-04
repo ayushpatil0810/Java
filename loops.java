e0d98101979fa2d0704fcdff7c62a65f0a64de59
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-04 23:45:28 +0530

    Add loops.java with commented examples of for, while, and do-while loops

---
+++ loops.java
@@ -0,0 +1,22 @@
+public class loops {
+    
+    public static void main(String[] args) {
+
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.println(i);
+        // }
+
+
+        // int k = 1;
+
+        // while (k <= 5) {
+        //     System.out.println(k);
+        //     k++;
+        // }
+
+        // do {
+        //     System.out.println(k);
+        //     k++;
+        // } while (k <= 0);
+    }
+}
