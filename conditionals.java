58b7393d1b7e75babd4547ab7741de710d5eba78
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-04 22:43:56 +0530

    Add conditionals.java with examples of conditional statements

---
+++ conditionals.java
@@ -0,0 +1,17 @@
+public class conditionals {
+    public static void main(String[] args) {
+        int age = 19;
+        int y = 30;
+
+        // if(age > 18 && age <= 20) 
+        //     System.out.println("Eligible for voting");
+        
+        if (age > y)
+            System.out.println(age);
+        else
+            System.out.println(y);
+
+        // && -> and operator
+        // || -> or operator
+    }
+}
