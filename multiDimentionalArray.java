8c881fcc7eccf38c668a936303f859cc3ff8458d
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-05 12:33:04 +0530

    Remove commented-out code and resolve merge conflict in multiDimentionalArray.java

--- multiDimentionalArray.java
+++ multiDimentionalArray.java
@@ -10,7 +10,6 @@ public class multiDimentionalArray {
         }
 
 
-<<<<<<< HEAD
         // for(int i = 0; i < 3; i++) {
         //     for(int j = 0; j < 4; j++) {
         //         System.out.print(nums[i][j] + " ");
@@ -21,11 +20,9 @@ public class multiDimentionalArray {
         for(int arr[] : nums) {
             for(int n : arr) {
                 System.out.print(n + " ");
-=======
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 4; j++) {
                 System.out.print(nums[i][j] + " ");
->>>>>>> fe53adb7b52ee290f88fec6f0e46f0031cb6b12a
             }
             System.out.println();
         }
