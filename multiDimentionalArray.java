715fc45fae68dbd554dd493b769f15427a4b33b5
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-05 12:37:43 +0530

    Fix formatting by adding missing braces in multiDimentionalArray.java

--- multiDimentionalArray.java
+++ multiDimentionalArray.java
@@ -20,6 +20,8 @@ public class multiDimentionalArray {
         for(int arr[] : nums) {
             for(int n : arr) {
                 System.out.print(n + " ");
+            }
+        }
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 4; j++) {
                 System.out.print(nums[i][j] + " ");
