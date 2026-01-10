13e86fb57a29dcdf9f5a528694bb2888b5a10bb8
Ayush Patil <94798136+ayushpatil0810@users.noreply.github.com>
2026-01-10 13:24:58 +0530

    Add staticKeyword.java with Mobile class demonstrating static variable usage

---
+++ staticKeyword.java
@@ -0,0 +1,31 @@
+class Mobile {
+    String brand;
+    int price;
+    String network;
+    // static String name = "Smartphone";
+    static String name;
+
+    public void show() {
+        System.out.println(brand + " : " + price + " : " + name);
+    }
+}
+
+public class staticKeyword {
+    public static void main(String[] args) {
+
+        Mobile.name = "Smartphone";
+
+        Mobile obj1 = new Mobile();
+        obj1.brand = "Apple";
+        obj1.price = 1500;
+        // obj1.name = "SmartPhone";
+        
+        Mobile obj2 = new Mobile();
+        obj2.brand = "Samsung";
+        obj2.price = 1700;
+        // obj2.name = "SmartPhone";
+        
+        obj1.show();
+        obj2.show();
+    }
+}
